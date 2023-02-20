f79c27cb6ba3d180106271184143b3a20b07449a
allisonkwan <akwan2020@gmail.com>
2023-02-20 15:17:49 -0500

    Add line chart

--- App.js
+++ App.js
@@ -1,12 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { MyLineChart } from './components/MyLineChart';
 
 export default function App() {
+
+  const testData = {
+    labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+    datasets: [
+      {
+        data: [830, 762, 810, 700, 723, 493, 677, 641, 509, 213, 335, 198, 29]
+      },
+    ],
+  };
+  const currStockPrice = 151.23;
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>APPL</Text>
+      <Text>${currStockPrice}</Text>
       <StatusBar style="auto" />
+      <MyLineChart data={testData} />
     </View>
+
   );
 }
 
---
+++ components/MyLineChart.js
@@ -0,0 +1,24 @@
+import { View, Text, Dimensions } from 'react-native'
+import React from 'react'
+import { LineChart } from 'react-native-chart-kit'
+
+export function MyLineChart({ data }) {
+    return (
+        <View>
+            <Text>
+                My Line Chart
+            </Text>
+            <LineChart
+                data={data}
+                width={Dimensions.get('window').width}
+                height={200}
+                yAxisLabel={'$'}
+                chartConfig={{
+                    backgroundGradientFrom: 'darkblue',
+                    backgroundGradientTo: 'blue',
+                    color: (opacity = 3) => `rgba(255, 255, 255, ${opacity})`
+                }}
+            />
+        </View>
+    )
+}
