f3e3152c75cadac64a21f60b57dbd9fa2c8029ed
allisonkwan <akwan2020@gmail.com>
2023-02-21 10:30:13 -0500

    Add radio buttons

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import RadioGroup from 'react-native-radio-buttons-group';
+import style from './App.module.css';
 import { MyLineChart } from './components/MyLineChart';
 
 export default function App() {
@@ -13,23 +16,62 @@ export default function App() {
     ],
   };
   const currStockPrice = 151.23;
+  const radioButtonsData = [
+    {
+      id: '1', // acts as primary key, should be unique and non-empty string
+      label: '1D',
+      value: '1D',
+    },
+    {
+      id: '2',
+      label: '1W',
+      value: '1W',
+    },
+    {
+      id: '3',
+      label: '1M',
+      value: '1M',
+    },
+    {
+      id: '4',
+      label: '3M',
+      value: '3M',
+    },
+    {
+      id: '5',
+      label: '1Y',
+      value: '1Y',
+    },
+    {
+      id: '6',
+      label: '5Y',
+      value: '5Y',
+    },
+  ];
+  const [radioButtons, setRadioButtons] = useState('1D'); //pass in our data to this state. This will store the current user's choice
+  const setValue = (value) => {
+    var newArray = value.filter((item) => item.selected === true); //get the items that are selected
+    setRadioButtons(newArray[0].value); //set the selected value in this Hook
+  };
 
   return (
-    <View style={styles.container}>
-      <Text>APPL</Text>
-      <Text>${currStockPrice}</Text>
-      <StatusBar style="auto" />
-      <MyLineChart data={testData} />
-    </View>
+    <SafeAreaView style={style.container}>
+      <View>
+        <StatusBar style="auto" />
+        <Text>APPL</Text>
+        <Text>${currStockPrice}</Text>
 
+        <MyLineChart data={testData} />
+        <View>
+          <RadioGroup
+            radioButtons={radioButtonsData}
+            onPress={(value) => setValue(value)}
+            containerStyle={style.radio}
+            layout='row'
+          />
+          <Text>You selected: {radioButtons}</Text>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
